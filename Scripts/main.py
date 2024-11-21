3bc0d69dab4a780c580b0255ced767364dcab869
Baseer Faizan <baseer.faizan@gmail.com>
2024-11-21 19:11:13 +0000

    added get number of lines method

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,4 @@
+MAX_LINES = 3
 
 def deposit():
     while True:
@@ -12,6 +13,21 @@ def deposit():
             print("Please enter a number.")
 
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("How many lines would you like to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else
+                print("enter a valid number of lines.")
+        else:
+            print("Please enter a number")
+
+    return lines
+
 
 def main():
     balance = deposit()
