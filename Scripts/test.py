7cf63d850072ea7a0fbee8a0a5da4d1fc1642968
Baseer Faizan <baseer.faizan@gmail.com>
2024-11-21 18:57:15 +0000

    added deposit function and check

--- Scripts/test.py
+++ Scripts/test.py
@@ -2,3 +2,13 @@
 def deposit():
     while True:
         amount = input("what would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount >0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            print("Please enter a number.")
+
+    return amount
