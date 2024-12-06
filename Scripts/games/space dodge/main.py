2ff47e1f5628b80fbedd446c3e81b2c126b6b953
Baseer Faizan <baseer.faizan@gmail.com>
2024-12-06 05:54:53 +0000

    added rain, collision detection and end game screens

--- Scripts/games/space dodge/main.py
+++ Scripts/games/space dodge/main.py
@@ -11,18 +11,24 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpg"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
+RAIN_WIDTH = 10
+RAIN_HEIGHT = 20
 
 PLAYER_VELOCITY = 10
+RAIN_VELOCITY = 3
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
-def draw(player, elapsed_time):
+def draw(player, elapsed_time, rain_drops):
     WIN.blit(BG,(0,0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10,10))
 
     pygame.draw.rect(WIN, "red", player)
+
+    for rain in rain_drops:
+        pygame.draw.rect(WIN, "white", rain)
 
     pygame.display.update()
 
@@ -35,9 +41,25 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    rain_add_increment = 2000
+    rain_count = 0
+
+    rain_drops = []
+    hit = False
+
+
     while run:
-        clock.tick(60)
+        rain_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+
+        if rain_count > rain_add_increment:
+            for _ in range(3):
+                rain_x = random.randint(0, WIDTH - RAIN_WIDTH)
+                rain = pygame.Rect(rain_x, -RAIN_HEIGHT, RAIN_WIDTH, RAIN_HEIGHT)
+                rain_drops.append(rain)
+
+            rain_add_increment = max(200, rain_add_increment -50)
+            rain_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -50,8 +72,23 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VELOCITY + player.width <= WIDTH:
             player.x += PLAYER_VELOCITY
 
+        for rain in rain_drops[:]:
+            rain.y += RAIN_VELOCITY
+            if rain.y > HEIGHT:
+                rain_drops.remove(rain)
+            elif rain.y + rain.height >= player.y and rain.colliderect(player):
+                rain_drops.remove(rain)
+                hit = True
+                break
 
-        draw(player, elapsed_time)
+        if hit:
+            lost_text = FONT.render("You lost!", 1, "white")
+            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
+
+        draw(player, elapsed_time, rain_drops)
 
     pygame.quit()
 
