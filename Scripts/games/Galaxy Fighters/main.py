f417eaca4fa2e142ddd09eccbc34392dd718014b
Baseer Faizan <baseer.faizan@gmail.com>
2024-12-13 02:03:52 +0000

    added spaceships and clock for fps

--- Scripts/games/Galaxy Fighters/main.py
+++ Scripts/games/Galaxy Fighters/main.py
@@ -1,21 +1,36 @@
 import pygame
+import time
+import os
 pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Galaxy Fighters")
 
-def main():
+WHITE = (255, 255, 255)
 
+FPS = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+
+def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
-
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+        draw()
+
     pygame.quit()
 
+def draw():
+
+    WIN.fill(WHITE)
+    pygame.display.update()
 
 if __name__ == "__main__":
     main()
